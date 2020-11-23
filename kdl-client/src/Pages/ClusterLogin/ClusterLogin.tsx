3a8659ae46bc4667acad48b9ca20f42ecf745aa0
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2020-11-23 13:36:54 +0100

    feat: Redesign cluster login

--- kdl-client/src/Pages/ClusterLogin/ClusterLogin.tsx
+++ kdl-client/src/Pages/ClusterLogin/ClusterLogin.tsx
@@ -1,6 +1,6 @@
 import { Button, CHECK, TextInput } from 'kwc';
 import ROUTE, { RouteClusterParams } from 'Constants/routes';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import DefaultPage from 'Components/Layout/Page/DefaultPage/DefaultPage';
 import EmailIcon from '@material-ui/icons/Mail';
@@ -30,12 +30,12 @@ function ClusterLogin() {
   const [linkSent, setLinkSent] = useState(false);
   const { clusterId } = useParams<RouteClusterParams>();
   const { getCluster } = useClusters();
+  const emailReceiver = useRef('');
   const cluster = getCluster(clusterId);
 
   const {
     handleSubmit,
     setValue,
-    getValues,
     unregister,
     register,
     errors,
@@ -71,6 +71,7 @@ function ClusterLogin() {
   function onSubmit({ email }: FormData) {
     if (cluster) {
       ipcRenderer.send('clusterLogin', { clusterId: cluster.id, email });
+      emailReceiver.current = email;
     } else {
       console.error(`Cannot get cluster`);
     }
@@ -99,38 +100,40 @@ function ClusterLogin() {
       actions={actions}
     >
       <div className={styles.container}>
-        <div className={styles.name}>
-          <p className={styles.title}>CLUSTER NAME</p>
-          <div className={styles.content}>
-            <div className={styles.circle} />
-            <p className={styles.value}>{cluster?.name}</p>
+        <div className={styles.information}>
+          <div className={styles.url}>
+            <p className={styles.title}>CLUSTER URL</p>
+            <p className={styles.value} title={cluster?.url}>
+              {cluster?.url}
+            </p>
+          </div>
+          <div className={styles.name}>
+            <p className={styles.title}>CLUSTER NAME</p>
+            <div className={styles.content}>
+              <div className={styles.circle} />
+              <p className={styles.value}>{cluster?.name}</p>
+            </div>
           </div>
         </div>
-        <div className={styles.url}>
-          <p className={styles.title}>CLUSTER URL</p>
-          <p className={styles.value} title={cluster?.url}>
-            {cluster?.url}
-          </p>
-        </div>
-        <div className={styles.formEmail}>
-          <TextInput
-            label="email"
-            onChange={(value: string) => {
-              setError('email', {});
-              setValue('email', value);
-            }}
-            onEnterKeyPress={handleSubmit(onSubmit)}
-            error={errors.email?.message}
-            showClearButton
-            autoFocus
-          />
-        </div>
-        <div className={styles.buttons}>
+        <div className={styles.form}>
+          <div className={styles.formEmail}>
+            <TextInput
+              label="email"
+              onChange={(value: string) => {
+                setError('email', {});
+                setValue('email', value);
+              }}
+              onEnterKeyPress={handleSubmit(onSubmit)}
+              error={errors.email?.message}
+              showClearButton
+              autoFocus
+            />
+          </div>
           <div className={cx(styles.confirmation, { [styles.show]: linkSent })}>
             <div className={styles.icon}>
               <EmailIcon className="icon-regular" />
             </div>
-            <p>{`link sent to "${getValues().email}". Check your inbox.`}</p>
+            <p>{`link sent to "${emailReceiver.current}". Check your inbox.`}</p>
           </div>
         </div>
       </div>

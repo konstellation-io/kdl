a085e71b108dda79e09707f4ec9a8a858f8c0b6e
Daniel Chavero Gaspar <daniel.chavero@intelygenz.com>
2021-01-21 16:26:23 +0100

    clean github pages branch

--- kdl-client/src/setupTests.js
+++
@@ -1,8 +0,0 @@
-import 'mutationobserver-shim';
-import '@testing-library/jest-dom/extend-expect';
-import 'jest-canvas-mock';
-
-import Adapter from 'enzyme-adapter-react-16';
-import { configure } from 'enzyme';
-
-configure({ adapter: new Adapter() });
--- kdl-client/store/store.js
+++
@@ -1,7 +0,0 @@
-const schema = require('./schema.js');
-const Store = require('electron-store');
-
-const store = new Store({ schema });
-// store.clear();
-
-module.exports = store;

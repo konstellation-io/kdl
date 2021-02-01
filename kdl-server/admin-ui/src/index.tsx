00e54d60c365f0d53b2f24e182f482a23676a65c
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2021-02-01 13:54:03 +0100

    Remove admin-ui/src/index.scss

--- kdl-server/admin-ui/src/index.tsx
+++ kdl-server/admin-ui/src/index.tsx
@@ -1,4 +1,3 @@
-import './index.scss';
 import 'kwc/index.css';
 import 'Styles/app.global.scss';
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,8 +5,8 @@ import 'react-tabs/style/react-tabs.css';
 import 'Styles/react-tabs.scss';
 import 'Styles/react-toastify.scss';
 
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));

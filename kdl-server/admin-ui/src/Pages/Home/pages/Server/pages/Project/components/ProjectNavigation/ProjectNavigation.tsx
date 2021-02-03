e1e849e05df88885b5868376d196df78cf1600d7
marco.gattelli <marco.gattelli@intelygenz.com>
2021-02-03 11:54:36 +0100

    TODO refactor

--- kdl-server/admin-ui/src/Pages/Home/pages/Server/pages/Project/components/ProjectNavigation/ProjectNavigation.tsx
+++ kdl-server/admin-ui/src/Pages/Home/pages/Server/pages/Project/components/ProjectNavigation/ProjectNavigation.tsx
@@ -19,7 +19,7 @@ const NavButtonLink: FC<any> = ({ children, ...props }) => (
 
 function ProjectNavigation() {
   const { serverId, projectId } = useParams<RouteProjectParams>();
-  // TODO: get the left navigation bar state from the local storage
+  // FIXME: get the left navigation bar state from the local storage
   const [opened, setOpened] = useState(false);
   const { togglePanel } = usePanel(PanelType.PRIMARY, {
     id: PANEL_ID.SETTINGS,
